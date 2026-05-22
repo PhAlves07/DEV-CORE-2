02be820e75e88e4b36bcbe93890dad545d10fadf
PhAlves07 <pedroh01072006@gmail.com>
2026-05-21 23:30:18 -0300

    efeito splash

--- agreste soluciona/dev-core/src/screen/Splash/index.tsx
+++ agreste soluciona/dev-core/src/screen/Splash/index.tsx
@@ -3,30 +3,117 @@ import {
   Text,
   Image,
   TouchableOpacity,
+  Animated,
 } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
+
+
+import React, {
+  useEffect,
+  useRef,
+} from 'react';
+
 export default function SplashScreen() {
   const navigation = useNavigation();
 
-  return (
-    <TouchableOpacity
-      style={styles.container}
-      activeOpacity={1}
-      onPress={() => navigation.navigate('Home' as never)}
-    >
-      <Image
-        source={require('../../assets/logo.png')}
-        style={styles.logo}
-        resizeMode="contain"
-      />
+  const translateY = useRef(
+    new Animated.Value(120)
+  ).current;
 
-      <Text style={styles.swipeText}>
-        Toque para continuar
-      </Text>
-    </TouchableOpacity>
+  const opacity = useRef(
+    new Animated.Value(0)
+  ).current;
+
+  const screenTranslateY = useRef(
+    new Animated.Value(0)
+  ).current;
+
+  const handlePress = () => {
+
+    Animated.timing(screenTranslateY, {
+      toValue: -900,
+      duration: 700,
+      useNativeDriver: true,
+    }).start(() => {
+
+      navigation.navigate('Home' as never);
+
+    });
+  };
+
+  useEffect(() => {
+
+    Animated.parallel([
+
+      Animated.timing(translateY, {
+        toValue: 0,
+        duration: 1200,
+        useNativeDriver: true,
+      }),
+
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 1200,
+        useNativeDriver: true,
+      }),
+
+    ]).start();
+
+  }, []);
+  return (
+    <Animated.View
+      style={{
+        flex: 1,
+        transform: [
+          {
+            translateY: screenTranslateY,
+          },
+        ],
+      }}
+    >
+
+      <TouchableOpacity
+        style={styles.container}
+        activeOpacity={1}
+        onPress={handlePress}
+      >
+
+        <Animated.View
+          style={{
+            alignItems: 'center',
+            opacity,
+
+            transform: [
+              {
+                translateY,
+              },
+            ],
+          }}
+        >
+
+          <Image
+            source={require('../../assets/logo.png')}
+            style={styles.logo}
+            resizeMode="contain"
+          />
+
+        </Animated.View>
+        <Animated.Text
+          style={[
+            styles.swipeText,
+            {
+              opacity,
+            },
+          ]}
+        >
+          Toque para continuar
+        </Animated.Text>
+
+      </TouchableOpacity>
+    </Animated.View>
   );
 }
